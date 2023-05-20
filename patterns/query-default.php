9ea73656ce65a285751dd2e763e7ab0230ba6447
Justin Tadlock <justintadlock@gmail.com>
2023-05-20 18:45:05 -0500

    Template cleanup
    Brings consistency to the block template design so that it's more flexible for customization. Removes the Front Page: Hero Grid pattern since it was being used as a placeholder pattern for a time. Introduces the Query: Default pattern as a real-world example for theme authors.

---
+++ patterns/query-default.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: Query: Default
+ * Slug: first-draft/query-default
+ * Categories: theme, query
+ * Block Types: core/query
+ */
+?>
+<!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"tagName":"div","layout":{"type":"constrained"}} -->
+<div class="wp-block-query">
+
+	<!-- wp:post-template -->
+		<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+		<article class="wp-block-group">
+			<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+			<header class="wp-block-group">
+				<!-- wp:post-date /-->
+				<!-- wp:post-title {"isLink":true} /-->
+			</header>
+			<!-- /wp:group -->
+			<!-- wp:post-excerpt {"moreText":"Continue reading →","showMoreOnNewLine":false} /-->
+		</article>
+		<!-- /wp:group -->
+	<!-- /wp:post-template -->
+
+	<!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"space-between"}} -->
+		<!-- wp:query-pagination-previous /-->
+		<!-- wp:query-pagination-numbers /-->
+		<!-- wp:query-pagination-next /-->
+	<!-- /wp:query-pagination -->
+
+</div>
+<!-- /wp:query -->

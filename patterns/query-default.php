38554260bc1e1c9f2cce6f8a636a2ffa196549f9
Justin Tadlock <justintadlock@gmail.com>
2023-05-21 12:41:15 -0500

    Wrangling default query layout
    Makes the default query layout more flexible for users to customize the inner parts.

--- patterns/query-default.php
+++ patterns/query-default.php
@@ -6,23 +6,28 @@
  * Block Types: core/query
  */
 ?>
-<!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"tagName":"div","layout":{"type":"constrained"}} -->
-<div class="wp-block-query">
+<!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-query alignfull">
 
-	<!-- wp:post-template -->
+	<!-- wp:post-template {"align":"full"} -->
 		<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
 		<article class="wp-block-group">
-			<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-			<header class="wp-block-group">
+
+			<!-- wp:group {"tagName":"header","align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+			<header class="wp-block-group alignfull">
+
 				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"lg"} -->
 				<div class="wp-block-group has-lg-font-size">
 					<!-- wp:post-date /-->
 				</div>
 				<!-- /wp:group -->
+
 				<!-- wp:post-title {"isLink":true} /-->
 			</header>
 			<!-- /wp:group -->
+
 			<!-- wp:post-excerpt {"moreText":"Continue reading →","showMoreOnNewLine":false} /-->
+
 		</article>
 		<!-- /wp:group -->
 	<!-- /wp:post-template -->

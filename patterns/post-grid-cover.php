0f133d85dce959493d403bd227b2e0efca35ccb5
Justin Tadlock <justintadlock@gmail.com>
2023-05-25 14:31:15 -0500

    Add min-height to cover in Post Grid: Cover

--- patterns/post-grid-cover.php
+++ patterns/post-grid-cover.php
@@ -20,8 +20,8 @@
 			<!-- wp:group {"tagName":"article","layout":{"type":"constrained"}} -->
 			<article class="wp-block-group">
 
-				<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"contentPosition":"bottom left","isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
-				<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-bottom-left" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
+				<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"minHeight":32,"minHeightUnit":"rem","contentPosition":"bottom left","isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
+				<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-bottom-left" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80);min-height:32rem">
 					<span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
 					<div class="wp-block-cover__inner-container">
 
